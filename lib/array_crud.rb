bff3d2eb3911278a73abfc12a22a337ba0a515e5
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-09 02:12:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,28 +12,44 @@ end
 
 def add_element_to_end_of_array(array, element)
   
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
+  
+  bands << ""
+  
 end
 
 def add_element_to_start_of_array(array, element)
+  
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
   
 end
 
 def remove_element_from_end_of_array(array)
   
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
+  
 end
 
 def remove_element_from_start_of_array(array)
+  
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
   
 end
 
 def retrieve_element_from_index(array, index_number)
   
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
+  
 end
 
 def retrieve_first_element_from_array(array)
   
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
+  
 end
 
 def retrieve_last_element_from_array(array)
+  
+  bands = ["AFI", "Thirty Seconds to Mars", "The Killers", "Nirvana"]
   
 end
